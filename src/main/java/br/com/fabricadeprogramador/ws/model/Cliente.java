2543d8c54e4106abdc10c4a64675bcedf4fc8c79
virmerson <virmerson@hightechcursos.com.br>
2016-06-11 09:44:00 -0400

    Desacoplando ClienteController, ClienteService, ClienteRepository parte 2

--- src/main/java/br/com/fabricadeprogramador/ws/model/Cliente.java
+++ src/main/java/br/com/fabricadeprogramador/ws/model/Cliente.java
@@ -1,7 +1,14 @@
 package br.com.fabricadeprogramador.ws.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private String nome;
 	public Integer getId() {
