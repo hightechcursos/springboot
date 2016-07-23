ef00d0b8a2823f88fc423d10048087ef2ce5b70e
virmerson <virmerson@hightechcursos.com.br>
2016-07-23 10:57:16 -0400

    Aula 07 parte 02

--- src/main/java/br/com/fabricadeprogramador/ws/controller/LoginController.java
+++ src/main/java/br/com/fabricadeprogramador/ws/controller/LoginController.java
@@ -1,5 +1,9 @@
 package br.com.fabricadeprogramador.ws.controller;
 
+import java.util.Date;
+
+import javax.servlet.ServletException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fabricadeprogramador.ws.model.Usuario;
 import br.com.fabricadeprogramador.ws.service.UsuarioService;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @RestController
 public class LoginController {
@@ -19,11 +26,39 @@ public class LoginController {
 	private UsuarioService usuarioService;
 	
 	@RequestMapping(value="/autenticar", consumes=MediaType.APPLICATION_JSON_VALUE, method= RequestMethod.POST)
-	public ResponseEntity<Usuario> autenticar(@RequestBody  Usuario usuario){
-		System.out.println(usuario.getNome() + " " + usuario.getSenha());
+	public LoginResponse autenticar(@RequestBody  Usuario usuario) throws ServletException{
+		
+		if (usuario.getNome() ==null || usuario.getSenha()==null){
+			throw new ServletException("Nome e senha obrigatório.");
+		}
+		
 		//Consulta no banco
 		Usuario usuAutenticado = usuarioService.buscarPorNome(usuario.getNome());
 		
-		return new ResponseEntity<Usuario>(usuAutenticado, HttpStatus.OK);
+		if (usuAutenticado==null){
+			throw new ServletException("Usuário não encontrado.");
+		}
+		
+		if (!usuAutenticado.getSenha().equals(usuario.getSenha())){
+			throw new ServletException("Usuário ou senha inválido.");
+		}
+		
+		String token=  Jwts.builder()
+				.setSubject(usuAutenticado.getNome())
+				.signWith(SignatureAlgorithm.HS512, "banana")
+				.setExpiration(new Date(System.currentTimeMillis() + 1 * 60 * 1000))
+				.compact();
+		
+		
+		return	new LoginResponse(token);
+	}		
+	
+	private class LoginResponse{
+		public String token;
+		 
+		public LoginResponse(String token ){
+			this.token=token; 
+		 }
+		
 	}
 }
